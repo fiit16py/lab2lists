a768c0fe63f00d805e01a848fc6c13d447210938
enuvin <e_unarov@mail.ru>
2019-10-09 19:12:35 +0900

    lab2lists - 2 updated+

--- task2.py
+++ task2.py
@@ -1,11 +1,11 @@
 def max_even(A):
-    maxn=-1
+    maxn=min(A)
     for i in A:
         if i%2==0:
             if maxn<i:
                 maxn=i
-    if maxn==-1:
-         return "false"
+    if maxn==min(A):
+         return "None"
     else:  return maxn
 if __name__ == '__main__':
 	  A = list(map(int, input().split()))
