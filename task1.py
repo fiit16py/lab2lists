bbbec46aa774ec838693ae2be80781c4530810b0
enuvin <e_unarov@mail.ru>
2019-10-09 18:25:03 +0900

    lab2lists - 1

--- task1.py
+++ task1.py
@@ -1,6 +1,10 @@
 def average(A):
-    1
-    return  2
+    s=0
+    k=0
+    for i in A:
+        s+=i
+        k+=1
+    return s/k
 
 if __name__ == '__main__':
 	  A = list(map(int, input().split()))
