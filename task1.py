db2e0b53e5a5633e075ecc9fd91809422a0b8f60
arsen andreev <zeroyak@gmail.com>
2019-10-09 18:13:04 +0900

    za alliance!

--- task1.py
+++ task1.py
@@ -1,6 +1,7 @@
 def average(A):
-    return 0
+    av = 0
+    
 
 if __name__ == '__main__':
-	  A = list(map(int, input().split()))
-	  print(average(A))
+    A = list(map(int, input().split()))
+    print(average(A))
