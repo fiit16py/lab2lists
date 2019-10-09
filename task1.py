d9328c4dc16d5c859121a7be131f029b58a06f58
lizafnss <lizafnss@gmail.com.githab.com>
2019-10-09 19:04:38 +0900

    start work

--- task1.py
+++ task1.py
@@ -1,9 +1,8 @@
 def average(A):
-	sum=0
+	sum = 0
 	for i in A:
-	sum += i
+	    sum += i
 		return sum/len(A)
-
 if __name__ ==  '__main__':
 	  A = list(map(int, input().split()))
 	  print(average(A))
