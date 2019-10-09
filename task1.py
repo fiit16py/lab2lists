671f6cace3014271ef9a56e62a69c049f2465fdc
AlbertoMalvius <43897701+AlbertoMalvius@users.noreply.github.com>
2019-10-09 18:25:44 +0900

    Task1 done

--- task1.py
+++ task1.py
@@ -1,6 +1,10 @@
 def average(A):
-    return 0
+    summ=0
+    for i in range(len(A)):
+        summ=summ+A[i]
+    summ=summ/len(A)
+    return summ
 
 if __name__ == '__main__':
-	  A = list(map(int, input().split()))
-	  print(average(A))
+    A = list(map(int, input().split()))
+    print(average(A))
