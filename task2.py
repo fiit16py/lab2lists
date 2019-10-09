e8ec230d6775f83b393381f46c25f872f9468de8
LATRio <AlSenya@yandex.ru>
2019-10-09 18:35:49 +0900

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,6 +1,10 @@
 def max_even(A):
-    return None
+	mx = 0
+	for i in A:
+		if i % 2 == 0:
+			mx = max(mx, i)
+	return mx
 
 if __name__ == '__main__':
-	  A = list(map(int, input().split()))
-	  print(max_even(A))
+	A = list(map(int, input().split()))
+	print(max_even(A))
