1df6dbeb5a7d4d7860a2d5cf9b6f9064f6fc87f8
Maxim Xenofontov <ramutra1996@gmail.com>
2019-10-09 18:58:49 +0900

    task2 done

--- task2.py
+++ task2.py
@@ -1,6 +1,17 @@
 def max_even(A):
-    return None
-
+	j=0
+	max=A[0]
+	for i in A:
+		if j==1:
+			max=A[1]
+		if (j+1)%2==0:
+			if max<A[j]:
+				max=A[j]
+		j+=1
+	if j>1:
+		return max
+	else:
+		return None
 if __name__ == '__main__':
 	  A = list(map(int, input().split()))
 	  print(max_even(A))
