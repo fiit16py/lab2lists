3c570f63226f3a9b6b6f3c202f462ce56ec0bf19
Lana Dyagileva <43055444+dyaglana@users.noreply.github.com>
2019-10-09 19:16:51 +0900

    t2

--- task2.py
+++ task2.py
@@ -1,6 +1,10 @@
 def max_even(A):
-    return None
+    me = None
+    for a in A:
+        if a%2==0 and (me is None or a>me):  
+            me = a
+    return me
 
 if __name__ == '__main__':
-	  A = list(map(int, input().split()))
-	  print(max_even(A))
+      A = list(map(int, input().split()))
+      print(max_even(A))
