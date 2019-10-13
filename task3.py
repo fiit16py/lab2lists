dddf473063e7ff95cc9a79adc488b20a8f6cb0b4
andreev arsen <zeroyak@gmail.com>
2019-10-13 12:22:11 +0900

    task3 done

--- task3.py
+++ task3.py
@@ -1,9 +1,9 @@
 def cut_swap(A,n):
-    A[0] = A[n]
-
+    A = A[n:] + A[:n]
+    return A
 
 if __name__ == '__main__':
     A = list(map(int, input().split()))
     dlina_pervogo_otrezka = int(input())
-    cut_swap(A, dlina_pervogo_otrezka)
+    A = cut_swap(A, dlina_pervogo_otrezka)
     print(A)
