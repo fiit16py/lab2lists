ae75150f6bdc86ca9fb9019ceef3fc3bb2b2bb7f
lizafnss <lizafnss@gmail.com.githab.com>
2019-10-16 19:12:16 +0900

    task2

--- task2.py
+++ task2.py
@@ -1,6 +1,12 @@
 def max_even(A):
-    return None
-
-if __name__ == '__main__':
-	  A = list(map(int, input().split()))
-	  print(max_even(A))
+    maxn=min()A
+    for i in A:
+        if i%2==0:
+            if maxn<i:
+                maxn=i
+    if maxn==min(A):
+        return "None"
+    else: return maxn
+if __name__== '__main__':
+    A =list(map(int,input().split()))
+    print(max_even(A))
