109fefdf527a7a4cc49173b2b2ca7b8d563e0fa7
arsen andreev <zeroyak@gmail.com>
2019-10-16 19:28:15 +0900

    working

--- task3.py
+++ task3.py
@@ -1,9 +1,15 @@
-def cut_swap(A,n):
-    A = A[n:] + A[:n]
-    return A
+def cut_swap(A,k):
+    #return A[n:] + A[:n]
+    n = len(A)
+    for i in range(n // 2):
+        A[i], A[n-1-i] = A[n-1-i], A[i]
+    for i in range((n-k)//2):
+        A[i], A[k-1-i] = A[k-1-i], A[i]
+    for i in range(-1,(k-n)//2,-1):
+        A[i], A[k-1-i] = A[k-1-i], A[i]
 
 if __name__ == '__main__':
     A = list(map(int, input().split()))
     dlina_pervogo_otrezka = int(input())
-    A = cut_swap(A, dlina_pervogo_otrezka)
+    cut_swap(A, dlina_pervogo_otrezka)
     print(A)
