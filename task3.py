534aa8dc4ace4504173e0fd774b317a55125d67d
Кыппыгыров Сарыал Васильевич <swofde@MacBook-Pro-Kyppygyrov.local>
2019-10-16 18:01:11 +0900

    task3

--- task3.py
+++ task3.py
@@ -1,6 +1,9 @@
 def cut_swap(A,n):
     for i in range(n):
+        A    for i in range(n):
         A[i], A[len(A) - n + i] = A[len(A) - n + i], A[i]
+        if len(A) / 2 != n:
+            A[i], A[i + n] = A[i + n], A[i]
 
 if __name__ == '__main__':
     A = list(map(int, input().split()))
