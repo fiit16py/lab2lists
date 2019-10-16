d86f7f111fff7e2006f22eea50d507c41f7c5b0d
AlbertoMalvius <43897701+AlbertoMalvius@users.noreply.github.com>
2019-10-16 19:17:46 +0900

    task3 done

--- task3.py
+++ task3.py
@@ -1,10 +1,15 @@
 def cut_swap(A,n):
-    for i in range(n):
-        A[n+i],A[i]=A[i],A[n+i]
-    print(list(A))
+    inf=0
+    for i in range (n//2):
+        A[n-i-1],A[i]=A[i],A[n-i-1]
+    print(A)
+    for i in range((len(A)-n)//2):
+        A[n+i],A[-i-1]=A[-i-1],A[n+i]
+    for i in range(len(A)//2):
+        A[i],A[-i-1]=A[-i-1],A[i]
+    print(A)
 
 if __name__ == '__main__':
     A = list(map(int, input().split()))
     n = int(input())
     print(cut_swap(A,n))
-
