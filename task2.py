10559cb514fe0d6e2cb0d550f26a98298f4660d7
VasilevSaryal <saryal.basilev@mail.ru>
2019-10-23 13:26:18 +0900

    task2 DONE

--- task2.py
+++ task2.py
@@ -1,13 +1,10 @@
 def max_even(A):
-    B = A[:]
-    for i in A:
-        if i % 2 == 1:
-            B.remove(i)
-    if len(B)==0:
-        return None
-    else:
-        return max(B)
+	A.sort(reverse=True)
+	for i in A:
+		if i % 2 == 0:
+		    return i
+	return "None"
 
 if __name__ == '__main__':
-	  A = list(map(int, input().split()))
-	  print(max_even(A))
+	A = list(map(int, input().split()))
+	print(max_even(A))
