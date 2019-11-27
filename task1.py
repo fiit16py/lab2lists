6014d45931610d4186e8faad3bedadaa6696fd0f
eugeniy <43114426+enuvin@users.noreply.github.com>
2019-11-27 12:29:19 +0900

    upd

--- task1.py
+++ task1.py
@@ -1,10 +1,8 @@
 def average(A):
     s=0
-    k=0
     for i in A:
         s+=i
-        k+=1
-    return s/k
+    return s/len(A)
 
 if __name__ == '__main__':
 	  A = list(map(int, input().split()))
